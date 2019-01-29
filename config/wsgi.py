b7456503699533f092976e6d891609098a4ea0e2
JamieWilliamsHackIT <mail@jamiewilliams.co.uk>
2019-01-29 18:00:36 +0000

    Update wsgi settings

--- config/wsgi.py
+++ config/wsgi.py
@@ -15,6 +15,7 @@ framework.
 """
 import os
 import sys
+from config.settings.base import env
 
 from django.core.wsgi import get_wsgi_application
 
@@ -29,13 +30,13 @@ if os.environ.get('DJANGO_SETTINGS_MODULE') == 'config.settings.production':
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings.production"
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", env('DJANGO_SETTINGS_MODULE'))
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 application = get_wsgi_application()
-if os.environ.get('DJANGO_SETTINGS_MODULE') == 'config.settings.production':
+if env('DJANGO_SETTINGS_MODULE') == 'config.settings.production':
     application = Sentry(application)
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
