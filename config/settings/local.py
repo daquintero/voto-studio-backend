8866775f9874c7fc017547f14975fa7799b9f71b
JamieWilliamsHackIT <mail@jamiewilliams.co.uk>
2019-02-09 13:42:04 +0000

    Add pandas to requirements.txt

--- config/settings/local.py
+++ config/settings/local.py
@@ -7,7 +7,7 @@ from .base import env
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
-DEBUG = False
+DEBUG = True
 TESTING = False
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env('DJANGO_SECRET_KEY', default='bTQAYpxY4cfIIJbe4lJtoOdPgScrFJCWIqcSndXL5qd0OcsDvvDKIbYNTe7NTgDa')
