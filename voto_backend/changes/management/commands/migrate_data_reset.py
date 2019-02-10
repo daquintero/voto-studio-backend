284bc7dfe391d4a6d714d50caf16b7d71d8c55ed
JamieWilliamsHackIT <mail@jamiewilliams.co.uk>
2019-02-10 14:07:23 +0000

    Add delay

--- voto_backend/changes/management/commands/migrate_data_reset.py
+++ voto_backend/changes/management/commands/migrate_data_reset.py
@@ -1,3 +1,5 @@
+import time
+
 from django.conf import settings
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
@@ -7,4 +9,5 @@ from data.regex import migrate
 class Command(BaseCommand):
     def handle(self, *args, **options):
         call_command('full_reset', bypass=True)
+        time.sleep(10)
         call_command('bulk_indexing', 'default', bypass=True)
