1fef3cded74b0e7dd29d58bde8cd8822513c2eff
JamieWilliamsHackIT <mail@jamiewilliams.co.uk>
2019-03-20 00:45:39 +0000

    Add FinancielItem to admin panels

--- voto_studio_backend/corruption/admin.py
+++ voto_studio_backend/corruption/admin.py
@@ -3,4 +3,5 @@ from config.admin import register_models
 register_models(app_label='corruption', models={
     'InformativeSnippet': ['default', 'main_site'],
     'CorruptionCase': ['default', 'main_site'],
+    'FinancialItem': ['default', 'main_site'],
 })
